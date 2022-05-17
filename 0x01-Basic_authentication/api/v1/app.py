66d6ec61d7c824582611f83ee7071fc4a9a78bd0
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 20:10:58 +0300

    implement the authentication method

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -3,6 +3,7 @@
 Route module for the API
 """
 from os import getenv
+from api.v1.auth.auth import Auth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -12,6 +13,9 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
+if os.getenv('AUTH_TYPE', 'auth'):
+    auth = Auth()
 
 
 @app.errorhandler(404)
@@ -31,6 +35,18 @@ def request_unauthorized(error) -> str:
 def access_forbidden(error) -> str:
     """ Resource forbidden error handler """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@app.before_request
+def authenticate():
+    """ authenticates before making a request """
+    if auth and auth.require_auth(request.path, ['/api/v1/status/',
+                                                 '/api/v1/unauthorized/',
+                                                 '/api/v1/forbidden/']):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 if __name__ == "__main__":
