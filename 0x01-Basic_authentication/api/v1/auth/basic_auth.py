d4ab752f059faee2a6c0bc197f2c5308f1aac3c7
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 20:17:42 +0300

    add the class to implement Basic Auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+    Demonstrating Basic Auth
+"""
+
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ class that implements the Basic Auth system """
+    pass
