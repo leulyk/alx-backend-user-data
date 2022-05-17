f612be1f12ca4713cfcc24d0e57d932337bc3196
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 19:24:43 +0300

    add the skeleton of the Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+    Handles the API authentication
+"""
+
+
+from Flask import request
+from Typing import List, TypeVar
+
+
+class Auth:
+    """ Class to act as an authentication template """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ To be implemented soon """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ To be implemented soon """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ To be implemented soon """
+        return None
