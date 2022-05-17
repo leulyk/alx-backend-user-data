98cea06e153b2560a71752929f78ae727c0dc2ef
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 21:43:17 +0300

    handle * at the end of excluded paths

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,8 +15,13 @@ class Auth:
         """ checks for paths that require authentication """
         if path is not None and path[-1] != '/':
             path += '/'
-        if path is None or excluded_paths is None or \
-                path not in excluded_paths:
+        if path is None or excluded_paths is None:
+            return True
+        if path not in excluded_paths:
+            for ex_path in excluded_paths:
+                if ex_path.endswith('*'):
+                    if path.startswith(ex_path[:-1]):
+                        return True
             return True
         return False
 
