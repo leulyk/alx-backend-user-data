0cac6b96447a69f2f8a6c36d4fbb0de0e7f23b4e
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 21:48:05 +0300

    do not add / if * is at the end and fix the return if the wildcard matches

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -14,14 +14,15 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ checks for paths that require authentication """
         if path is not None and path[-1] != '/':
-            path += '/'
+            if path[-1] != '*':
+                path += '/'
         if path is None or excluded_paths is None:
             return True
         if path not in excluded_paths:
             for ex_path in excluded_paths:
                 if ex_path.endswith('*'):
                     if path.startswith(ex_path[:-1]):
-                        return True
+                        return False
             return True
         return False
 
