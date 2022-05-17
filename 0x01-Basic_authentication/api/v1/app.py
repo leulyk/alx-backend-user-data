c5403af813f6c48b0f2190abef904de929a76774
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 20:17:18 +0300

    add the Basic Auth option

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -4,6 +4,7 @@ Route module for the API
 """
 from os import getenv
 from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
@@ -16,6 +17,8 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 if os.getenv('AUTH_TYPE', 'auth'):
     auth = Auth()
+elif os.getenv('AUTH_TYPE', 'basic_auth'):
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
