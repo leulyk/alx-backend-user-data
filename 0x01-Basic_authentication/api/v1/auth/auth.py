5c26be397155b30d4159cc2d59078420beec1412
Leul Yiheyis <leulykpro@gmail.com>
2022-05-17 19:39:34 +0300

    implement the require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,12 @@ from typing import List, TypeVar
 class Auth:
     """ Class to act as an authentication template """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ To be implemented soon """
+        """ checks for paths that require authentication """
+        if path is not None and path[-1] != '/':
+            path += '/'
+        if path is None or excluded_paths is None or \
+                path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
