07da07e15b50c37aab0a9a1eb71b7401ddd9fa1f
Leul Yiheyis <leulykpro@gmail.com>
2022-05-25 19:34:03 +0300

    disable log and implementation of method to find a user based on parameters specified

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,8 +5,10 @@
 """
 
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base
@@ -19,7 +21,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -41,5 +43,19 @@ class DB:
             new_user.hashed_password = hashed_password
             self._session.add(new_user)
             self._session.commit()
-
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+            Searches for a user in the database and returns it
+            If no result found, raises NoResultFound exception
+            If wrong query arguments passed,
+                raises InvalidRequestError exception
+        """
+        for arg in kwargs:
+            if not hasattr(User, arg):
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
