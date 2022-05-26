d775e9aa191833af330b9ba300c9df66df3e1ca5
Leul Yiheyis <leulykpro@gmail.com>
2022-05-26 16:35:43 +0300

    implement the login functionality

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -5,7 +5,7 @@
 """
 
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import abort, Flask, jsonify, request
 
 
 AUTH = Auth()
@@ -31,6 +31,19 @@ def register_user():
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """ implements the login functionality """
+    email = request.form['email']
+    password = request.form['password']
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        resp = jsonify({"email": email, "message": "logged in"})
+        resp.set_cookie("session_id", session_id)
+        return resp
+    abort(401)
 
 
 if __name__ == ('__main__'):
