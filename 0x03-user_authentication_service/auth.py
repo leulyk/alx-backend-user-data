d64adc6efab0fa1891922d45676033804f62ea27
Leul Yiheyis <leulykpro@gmail.com>
2022-05-26 15:41:26 +0300

    implement the login validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -31,6 +31,14 @@ class Auth:
         self._db.add_user(email, hashed_passwd)
         return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ validates a user """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode(), user.hashed_password)
+
 
 def _hash_password(password: str) -> bytes:
     """
