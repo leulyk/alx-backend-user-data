f30b174b360c07d1f17347888aa0401d8c3b8e9c
Leul Yiheyis <leulykpro@gmail.com>
2022-05-27 05:09:34 +0300

    fix silly error

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,7 +49,7 @@ def login():
 @app.route('/sessions', methods=['DELETE'])
 def logout():
     """ implements the logout functionality """
-    session_id = resp.get_cookie("session_id")
+    session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(session_id)
     if user:
         AUTH.destroy_session(user.id)
