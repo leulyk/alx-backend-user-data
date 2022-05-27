6458e688f7caaa95b4e9adbfbfa07e018d3b6094
Leul Yiheyis <leulykpro@gmail.com>
2022-05-27 05:20:51 +0300

    implementation of method that resets password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,6 +73,16 @@ class Auth:
             return
         self._db.update_user(user.id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ resets a password """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        new_token = _generate_uuid()
+        self._db.update_user(reset_token=new_token)
+        return new_token
+
 
 def _hash_password(password: str) -> bytes:
     """
